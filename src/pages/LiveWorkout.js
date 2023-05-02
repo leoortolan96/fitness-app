13398451ac3cbe9c91dfe03e7d8c92b18b7dfb70
Leonardo Ortolan <leonardo.ortolan96@gmail.com>
2023-05-01 21:17:09 -0400

    task 1 - WIP dialog de alterar carga e criei o .env

--- src/pages/LiveWorkout.js
+++ src/pages/LiveWorkout.js
@@ -80,7 +80,9 @@ function LiveWorkoutPage() {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const response = await fetch("http://localhost:5000/live-workout/");
+        const response = await fetch(
+          process.env.REACT_APP_API_ENDPOINT + "/live-workout/"
+        );
         if (!response.ok) {
           throw Error("Failed to fetch resource");
         }
